ec9ce78ca06925a26ceb8777ea18ad5a96c57bcb
HuuChung98 <balechung93@gmail.com>
2023-10-15 15:17:50 +0700

    continues coding banner with slide show and text

--- src/modules/Home/components/Banner.jsx
+++ src/modules/Home/components/Banner.jsx
@@ -2,86 +2,53 @@ import React, { useEffect, useRef } from 'react'
 // import Swiper from 'swiper';
 
 const Banner = () => {
-  const swiperContainerRef = useRef(null);
-
-  useEffect(() => {
-
-    // Load Swiper script dynamically when the component mounts
-    const swiperScript = document.createElement('script');
-    swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js';
-    swiperScript.async = true;
-    swiperScript.onload = () => {
-      // Swiper script has loaded, you can initialize Swiper here
-      const swiper = new window.Swiper(swiperContainerRef.current, {
-        // Swiper options here
-        // Optional parameters
-        direction: 'vertical',
-        loop: true,
-
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
-      });
-      // Don't forget to destroy the Swiper instance when the component unmounts
-      return () => {
-        swiper.destroy();
-        // document.body.removeChild(swiperScript);
-      };
-    };
-
-    document.body.appendChild(swiperScript);
-
-    // Don't forget to destroy the Swiper instance when the component unmounts
-    return () => {
-      // swiper.destroy();
-      document.body.removeChild(swiperScript);
-    };
-
-  }, []);
-
 
   return (
-    <div className='container-swiper d-flex'>
-      <div className='swiper-image'>
-        {/* Slider main container */}
-        <div className="swiper" ref={swiperContainerRef}>
-          {/* Additional required wrapper */}
-          <div className="swiper-wrapper">
-            {/* Slides */}
-            <div className="swiper-slide background-image1">
-              <img src="/images/bg-highlight1.png" alt="" />
+    <div className='banner-container'>
+      <div className="row ">
+        <div className="slideShow-banner">
+          <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="/images/bg-highlight1.png" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="/images/bg-highlight2.png" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="/images/bg-highlight3.png" className="d-block w-100" alt="..." />
+              </div>
             </div>
-            <div className="swiper-slide background-image2">
-              <img src="/images/bg-highlight2.png" alt="" />
-            </div>
-            <div className="swiper-slide background-image3">
-              <img src="/images/bg-highlight3.png" alt="" />
-            </div>
-            ...
+            <button className="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </button>
           </div>
-          {/* If we need pagination */}
-          <div className="swiper-pagination" />
-          {/* If we need navigation buttons */}
-          <div className="swiper-button-prev" />
-          <div className="swiper-button-next" />
-          {/* If we need scrollbar */}
-          {/* <div className="swiper-scrollbar" /> */}
         </div>
-      </div>
-      <div className="swiper-text ">
-        <h2>Welconr</h2>
+        <div className="text-banner">
+          <div className='text-banner-showContent'>
+            <div className="header-text">
+              <h1> Travel &</h1>
+              <h1>Adventure</h1>
+            </div>
+            <div className="content">
+              <p>Sed convallis sit amet leo quis feugiat. Nunc interdum mollis facilisis. feugi Donec id the urna aliquet, suscipit turpis ut Donec id urna aliquet, suscipit turpis ut, facilisis purus.Sed convallis sit amet leo quis .</p>
+            </div>
+            <div className="detail d-flex">
+              <div className=''>
+                <button className='button-show'>View Adventure</button>
+              </div>
+              <div>
+                <button className='button-show'>Take a Tour</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
