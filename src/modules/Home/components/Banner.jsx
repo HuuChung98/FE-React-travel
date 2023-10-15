3b91dcb111178e154764ce8c7bdbcafe9ba968ee
HuuChung98 <balechung93@gmail.com>
2023-10-15 23:50:05 +0700

    Footer Component

--- src/modules/Home/components/Banner.jsx
+++ src/modules/Home/components/Banner.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef } from 'react'
+import "../../../Css/Home.css"
 // import Swiper from 'swiper';
 
 const Banner = () => {
 
   return (
     <div className='banner-container'>
-      <div className="row ">
+      <div className="row banner-content">
         <div className="slideShow-banner">
           <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
@@ -30,23 +31,78 @@ const Banner = () => {
           </div>
         </div>
         <div className="text-banner">
-          <div className='text-banner-showContent'>
-            <div className="header-text">
-              <h1> Travel &</h1>
-              <h1>Adventure</h1>
-            </div>
-            <div className="content">
-              <p>Sed convallis sit amet leo quis feugiat. Nunc interdum mollis facilisis. feugi Donec id the urna aliquet, suscipit turpis ut Donec id urna aliquet, suscipit turpis ut, facilisis purus.Sed convallis sit amet leo quis .</p>
-            </div>
-            <div className="detail d-flex">
-              <div className=''>
-                <button className='button-show'>View Adventure</button>
-              </div>
-              <div>
-                <button className='button-show'>Take a Tour</button>
-              </div>
+          <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                {/* <img src="..." className="d-block w-100" alt="..." /> */}
+                <div className='text-banner-showContent'>
+                  <div className="header-text">
+                    <h1> Travel &</h1>
+                    <h1>Adventure</h1>
+                  </div>
+                  <div className="content">
+                    <p>Sed convallis sit amet leo quis feugiat. Nunc interdum mollis facilisis. feugi Donec id the urna aliquet, suscipit turpis ut Donec id urna aliquet, suscipit turpis ut, facilisis purus.Sed convallis sit amet leo quis .</p>
+                  </div>
+                  <div className="detail d-flex">
+                    <div className=''>
+                      <button className='button-show'>View Adventure</button>
+                    </div>
+                    <div>
+                      <button className='button-show'>Take a Tour</button>
+                    </div>
 
+                  </div>
+                </div>
+
+              </div>
+              <div className="carousel-item">
+                <div className='text-banner-showContent'>
+                  <div className="header-text">
+                    <h1> Move The</h1>
+                    <h1>Earth</h1>
+                  </div>
+                  <div className="content">
+                    <p>Sed convallis sit amet leo quis feugiat. Nunc interdum mollis facilisis. feugi Donec id the urna aliquet, suscipit turpis ut Donec id urna aliquet, suscipit turpis ut, facilisis purus.Sed convallis sit amet leo quis .</p>
+                  </div>
+                  <div className="detail d-flex">
+                    <div className=''>
+                      <button className='button-show'>View Adventure</button>
+                    </div>
+                    <div>
+                      <button className='button-show'>Take a Tour</button>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className='text-banner-showContent'>
+                  <div className="header-text">
+                    <h1> Travel &</h1>
+                    <h1 className='show-h1'>Adventure</h1>
+                  </div>
+                  <div className="content">
+                    <p>Sed convallis sit amet leo quis feugiat. Nunc interdum mollis facilisis. feugi Donec id the urna aliquet, suscipit turpis ut Donec id urna aliquet, suscipit turpis ut, facilisis purus.Sed convallis sit amet leo quis .</p>
+                  </div>
+                  <div className="detail d-flex">
+                    <div className=''>
+                      <button className='button-show'>View Adventure</button>
+                    </div>
+                    <div>
+                      <button className='button-show'>Take a Tour</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+            <button className="carousel-control-prev" type="button" data-target="#carouselExampleFade" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-target="#carouselExampleFade" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </button>
           </div>
         </div>
       </div>
--- src/modules/Home/components/Footer.jsx
+++ src/modules/Home/components/Footer.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
+import "../../../Css/Home.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container '>
+      <div className="footer-above">
+        <div className='d-flex row'>
+          <div className="footer-left column">
+            <p>Duis rutrum nisl urna. Maecenas vel libero faucibus nisi venenatis hendrerit a id lectus. Suspendissendt molestie turpis nec lacinia vehicula.
+            </p>
+            <h2>
+              Follow Us On:
+            </h2>
+            <div>
+              <i className="fa-brands fa-instagram" />
+              <i class="fa-brands fa-facebook"></i>
+              <i class="fa-brands fa-pinterest-p"></i>
+              <i class="fa-solid fa-phone"></i>
+            </div>
+          </div>
+          <div className="footer-between d-flex column">
+            <div className='quick-link'>
+              <h2>
+                Quick Link
+              </h2>
+              <div>
+                <p>About Us</p>
+                <p>Tour Package</p>
+                <p>Destination</p>
+                <p>Tour Guide</p>
+                <p>Booking Process</p>
+                <p>Blog</p>
+              </div>
+            </div>
+            <div className="tour-type">
+              <h2>
+                Tour Type
+              </h2>
+              <div>
+                <p>Wild & Adventure Tours</p>
+                <p> Group Tour</p>
+                <p> Seasonal Tours</p>
+                <p>Relaxation Tours</p>
+                <p> Family Friendly Tours</p>
+              </div>
+            </div>
+          </div>
+          <div className="footer-right column">
+            <div>
+              <h3>
+                Gallery
+              </h3>
+              <div className='gallery-content'>
+                <div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-1.png" />
+                  </div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-2.png" />
+                  </div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-3.png" />
+                  </div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-4.png" />
+                  </div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-5.png" />
+                  </div>
+                  <div className="gallery-item">
+                    <img src="/images/fg-6.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='contact'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div className="footer-under">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
